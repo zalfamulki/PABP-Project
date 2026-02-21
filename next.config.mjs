ec5a95e0c5f9cef0f0ee45c19bfe4c2393f179bf
Zalfa <mulkizalpa@gmail.com>
2026-02-21 11:05:52 +0700

    menambahkan konfigurasi agar next.js mengizinkan gambar dari domain eksternal

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      { protocol: 'https', hostname: 'cdn.dummyjson.com' },
+      { protocol: 'https', hostname: 'i.dummyjson.com' },
+    ],
+  },
+};
+module.exports = nextConfig;
