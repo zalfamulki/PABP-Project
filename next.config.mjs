47c22cfd6b3559b16493ec985353059a00e954f3
Zalfa <mulkizalpa@gmail.com>
2026-02-21 21:04:49 +0700

    menhapus API yang tidak diperlukan

--- next.config.mjs
+++ next.config.mjs
@@ -4,8 +4,7 @@ const nextConfig = {
     remotePatterns: [
       { protocol: 'https', hostname: 'cdn.dummyjson.com' },
       { protocol: 'https', hostname: 'i.dummyjson.com' },
-      { protocol: 'https', hostname: 'image.api.playstation.com' },
-      { protocol: 'https', hostname: 'gmedia.playstation.com' },
+      
     ],
   },
 };
